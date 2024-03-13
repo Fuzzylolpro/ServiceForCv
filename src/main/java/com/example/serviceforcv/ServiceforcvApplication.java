eb354843bd2924065d203ab16784854d5e0fc43e
Алексей <fuzzylolpro@gmail.com>
2024-03-13 11:49:41 +0300

    go

---
+++ src/main/java/com/example/serviceforcv/ServiceforcvApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceforcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceforcvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceforcvApplication.class, args);
+    }
+
+}
